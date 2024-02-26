9b8dddd1a74d2046811ece2bc5e065de660c7573
mavr1ke <mainstip@mail.ru>
2024-02-26 22:29:50 +0100

    update

--- src/test/java/com/ait/qa31/hw5/AddProductToCartTests.java
+++ src/test/java/com/ait/qa31/hw5/AddProductToCartTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class AddProductToCart extends TestCase {
+public class AddProductToCartTests extends TestCase {
 
     @BeforeMethod
     public void precondition() {
