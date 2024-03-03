91018d3b2e94dcbf759269a49d222e7ee4b88833
mavr1ke <mainstip@mail.ru>
2024-03-03 21:34:17 +0100

    update

--- src/test/java/com/ait/qa31/hws/CreateAccountTests.java
+++ src/test/java/com/ait/qa31/hws/CreateAccountTests.java
@@ -1,21 +1,44 @@
 package com.ait.qa31.hws;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateAccountTests extends TestBase {
 
-    @Test
-    public void createAccountPositiveTest() {
+    @Test(dataProvider = "registrationData")
+    public void createAccountPositiveTest(String firstName, String lastName, String email, String password, String confirmPassword) {
         clickOnRegisterLink();
         fillRegisterForm(new User()
-                .setFirstName("Manuel")
-                .setLastName("Ferraro")
-                .setEmail("okay@gm.com")
-                .setPassword("qwerty123")
-                .setConfirmPassword("qwerty123"));
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setPassword(password)
+                .setConfirmPassword(confirmPassword));
         clickOnRegisterButton();
         Assert.assertTrue(isLogOutButtonPresent());
+    }
+
+    @DataProvider(name = "registrationData")
+    public Object[][] getRegistrationData() throws IOException {
+        List<Object[]> data = new ArrayList<>();
+        String csvFile = "src/test/resources/register_data.csv";
+
+        String line;
+        String csvSplitBy = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                String[] userData = line.split(csvSplitBy);
+                data.add(userData);
+            }
+        }
+        return data.toArray(new Object[0][]);
     }
 
 }
